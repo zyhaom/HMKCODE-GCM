750e3c566998cc144b4df47d9f7ee5fac9bfa9f0
Christopher Lamm <chris@theantichris.com>
2014-09-30 15:37:36 -0400

    Clean up.

--- app/src/main/java/com/eventbooking/android/gcm/MainActivity.java
+++ app/src/main/java/com/eventbooking/android/gcm/MainActivity.java
@@ -11,7 +11,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
