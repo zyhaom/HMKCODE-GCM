c2f4f74ea7f865a702346fccee88f3b773b0c057
Christopher Lamm <chris@theantichris.com>
2014-09-30 12:06:44 -0400

    onReceive() method.

--- app/src/main/java/com/eventbooking/android/gcm/GcmBroadcastReceiver.java
+++ app/src/main/java/com/eventbooking/android/gcm/GcmBroadcastReceiver.java
@@ -1,11 +1,19 @@
 package com.eventbooking.android.gcm;
 
+import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
 
 public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
     @Override public void onReceive(Context context, Intent intent) {
+        // Explicitly specify that GcmMessageHandler will handle the intent.
+        ComponentName componentName = new ComponentName(context.getPackageName(), GcmMessageHandler.class.getName());
 
+        // Start the service, keeping the device awake while it is launching.
+        startWakefulService(context, (intent.setComponent(componentName)));
+
+        setResultCode(Activity.RESULT_OK);
     }
 }
