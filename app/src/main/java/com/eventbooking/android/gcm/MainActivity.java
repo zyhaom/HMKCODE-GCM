f00a350511d8706b657017b56c5382b9eb5c2a59
Christopher Lamm <chris@theantichris.com>
2014-09-30 11:42:29 -0400

    On click handler for buttonGetRedId.

--- app/src/main/java/com/eventbooking/android/gcm/MainActivity.java
+++ app/src/main/java/com/eventbooking/android/gcm/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -23,6 +24,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonRegId = (Button) findViewById(R.id.buttonGetRegId);
+        editTextRegId = (EditText) findViewById(R.id.editTextRegId);
     }
 
     @Override
@@ -41,5 +45,9 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
 
         return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View view){
+
     }
 }
