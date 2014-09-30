695b47ebaba6f41b4bec2424bde663f9be6151e7
Christopher Lamm <chris@theantichris.com>
2014-09-30 16:53:03 -0400

    Clean up.

--- app/src/main/java/com/eventbooking/android/gcm/MainActivity.java
+++ app/src/main/java/com/eventbooking/android/gcm/MainActivity.java
@@ -22,7 +22,6 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
